b6f4c98d44ba26bf2ca57368b70fa40bbe5f9e42
Wanderson Elias <wandersonelias55@gmail.com>
2024-03-06 17:24:37 -0300

    Setup configuração de middlewares de erros

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,25 @@
 const express = require('express');
+require('express-async-errors');
 const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
+//middlewares configurações 
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+// middlewares de processamento
+app.use('/error',(req,res,next)=>{
+    throw new Error('Erro louco')
+})
 
 app.use('/',(req,res,next)=>{
     res.send("Rsposta Hello World");
 })
 
+//middleware de erros
 
-
-
-
+const errorMiddleware = require('./middlewares/errorMiddleware');
+app.use(errorMiddleware);
 module.exports = app;
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
 
-app.listen('8080',()=>{
+app.listen(parseInt(process.env.PORT),()=>{
     console.log("Server is runnig");
 })
